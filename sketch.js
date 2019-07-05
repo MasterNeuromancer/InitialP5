8014a679d8f744aba69188e6845df540d183c805
Neuromancer93 <itrejonumber1@gmail.com>
2019-07-04 23:01:32 -0700

    started cat whiskers, recolored canvas

--- sketch.js
+++ sketch.js
@@ -1,14 +1,21 @@
 function setup() {
 	createCanvas(400, 400);
-	background(150);
+	background(255);
 }
 
 function draw() {
-	var c = color(0);
-	fill(c);
+	fill(0);
+	let p2 = { x: 73, y: 24 };
+	let p3 = { x: 73, y: 61 },
+		p4 = { x: 15, y: 65 };
+	stroke(0);
 	circle(200, 150, 150);
 	triangle(134, 115, 167, 85, 125, 60);
 	triangle(266, 115, 233, 85, 275, 60);
 	fill('#B97C6D');
 	triangle(200, 160, 205, 150, 195, 150);
+	noFill();
+	stroke(160);
+	curve(170, 162, 188, 155, 120, 162, 135, 167);
+	curve(230, 162, 212, 155, 280, 162, 265, 167);
 }
